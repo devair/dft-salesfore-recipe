48db447f75773cd711000b1dfc7b1f467da1502a
Devair F Tonon <devairft@gmail.com>
2025-02-07 22:05:02 -0300

    tree-grid

---
+++ force-app/main/default/lwc/accountOnePage/accountOnePage.js
@@ -0,0 +1,11 @@
+import { LightningElement, api } from 'lwc';
+
+export default class AccountOnePage extends LightningElement {
+
+    @api recordId;
+    objectApiName = 'Account';
+ 
+    connectedCallback(){
+        console.log(this.recordId);
+    }
+}
---
+++ force-app/main/default/lwc/dataTree/dataTree.js
@@ -0,0 +1,98 @@
+import { LightningElement, wire, track } from 'lwc';
+import getAccountsWithContacts from '@salesforce/apex/AccountContactController.getAccountsWithContacts';
+import { NavigationMixin } from 'lightning/navigation';
+import FORM_FACTOR from '@salesforce/client/formFactor';
+
+const COLUMNS = [    
+    { 
+        label: 'Nome', 
+        fieldName: 'recordUrl', 
+        type: 'url', 
+        typeAttributes: { 
+            label: { fieldName: 'name' }, 
+            target: '_self',        
+        } 
+    },
+    { label: 'Telefone', fieldName: 'phone', type: 'phone' },
+    { label: 'Status', fieldName: 'status', type: 'text' },
+    { 
+        label: 'Ações', 
+        fieldName: 'id', 
+        type: 'button', 
+        typeAttributes: { 
+            label: 'One Page', 
+            name: 'onePage',
+            variant: 'brand' // Estilo azul Salesforce
+        } 
+    }
+];
+
+export default class DataTDataTreereeLWC extends NavigationMixin(LightningElement) {
+    
+    @track treeData = [];
+    @track currentExpandedRows;
+    isMobile = FORM_FACTOR === 'Small';
+    
+    columns = COLUMNS;
+
+    
+    @wire(getAccountsWithContacts)
+    wiredAccounts({ error, data }) {
+        if (data) {            
+            this.treeData = this.formatTreeData(data);            
+        } else if (error) {
+            console.error('Erro ao carregar contas:', error);            
+        }        
+    }
+
+    formatTreeData(accounts) {
+        
+        let treeData = accounts.map(account => ({
+            id: account.id,
+            name: account.name,                
+            phone: account.phone,
+            status: account.status,  
+            recordUrl: `/${account.id}`,
+            parentId: account.parentId,
+            _children: account.children?.map(child => ({
+                id: child.id,
+                name: child.name,
+                phone: child.phone,
+                status: child.status,
+                recordUrl: `/${child.id}`,
+                parentId: child.parentId,
+            }))
+        }));
+        
+        return treeData;
+    }
+
+    connectedCallback() {
+        console.log('The device form factor is: ' + FORM_FACTOR);
+    }
+  
+    handleRowAction(event) {
+        const recordId = event.detail.row.parentId;
+        const actionName = event.detail.action.name;
+
+        if (actionName === 'onePage') {
+            this.openLightningTab(recordId);
+        }
+    }
+
+    openLightningTab(recordId) {
+        let direction = {
+            componentDef: 'c:accountOnePage',
+            attributes: {
+                recordId: recordId
+            }
+        }
+
+        let encodeDef = btoa(JSON.stringify(direction));
+
+        this[NavigationMixin.Navigate]({
+            type: 'standard__webPage',
+            attributes: {url: '/one/one.app#'+encodeDef}
+        });
+    }
+}
